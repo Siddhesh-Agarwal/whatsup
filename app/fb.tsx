e36a08a4d792b60537bfc6b0112e6f5b5eddeb37
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2023-11-11 15:16:40 +0530

    first commit

---
+++ app/fb.tsx
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAIUlOnW5_spg4pCtno4yr2E7rO7YUSHHw",
+  authDomain: "whatsapp-clone-6b87a.firebaseapp.com",
+  projectId: "whatsapp-clone-6b87a",
+  storageBucket: "whatsapp-clone-6b87a.appspot.com",
+  messagingSenderId: "764436969466",
+  appId: "1:764436969466:web:abdd468cb7631f02161643",
+  measurementId: "G-QXGYBTS81L"
+};
+
+export default firebaseConfig;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import { initializeApp } from "firebase/app";
 import Image from 'next/image'
+import firebaseConfig from "./fb";
 
 export default function Home() {
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
